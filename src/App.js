eb309d42b5c19b9a6b6ef8cac86be90fac11a56d
Nafees Iqbal <33998031+iqbalnafee@users.noreply.github.com>
2021-07-16 20:23:18 +0600

    Income Expence added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 
 import './App.css';
-import Header from './components/Header'
-import Balance from './components/Balance'
+import Header from './components/Header';
+import Balance from './components/Balance';
+import IncomeExpense from './components/IncomeExpense'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Balance />
+      <IncomeExpense />
     </div>
   );
 }

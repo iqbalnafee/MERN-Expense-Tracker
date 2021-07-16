abff4b0381d837ea55fbf4cbfbe0e7e9d8d58013
Nafees Iqbal <33998031+iqbalnafee@users.noreply.github.com>
2021-07-16 20:43:37 +0600

    Add transaction created

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/Header';
 import Balance from './components/Balance';
 import IncomeExpense from './components/IncomeExpense';
 import TransactionList from './components/TransactionList';
-
+import AddTransaction from './components/AddTransaction';
 function App() {
   return (
     <div className="App">
@@ -12,6 +12,7 @@ function App() {
       <Balance />
       <IncomeExpense />
       <TransactionList />
+      <AddTransaction />
     </div>
   );
 }

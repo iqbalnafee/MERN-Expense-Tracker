f96a90a5c66607d0ddb87acdf7b88d638f2b9b93
Nafees Iqbal <33998031+iqbalnafee@users.noreply.github.com>
2021-07-16 20:48:49 +0600

    class changed to className

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -4,7 +4,7 @@ const Balance = () => {
     return (
         <div>
             <h4  style={{marginTop:"10px"}}>Your Balance</h4>
-            <h1 id="balance">$0.00</h1>
+            <h1 >$0.00</h1>
         </div>
     )
 }

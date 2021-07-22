66df840f7a013c3b7f3c2bc67028ba360b6b43a2
Nafees Iqbal <33998031+iqbalnafee@users.noreply.github.com>
2021-07-22 10:43:09 +0600

    Global context created

---
+++ src/context/GlobalState.js
@@ -0,0 +1,24 @@
+import React ,{ createContext,useReducer } from 'react';
+
+//initial State
+const initialState = {
+
+    transactions: [
+
+        { id: 1, text: 'Flower', amount: -20 },
+        { id: 2, text: 'Salary', amount: 300 },
+        { id: 3, text: 'Book', amount: -10 },
+        { id: 4, text: 'Camera', amount: 150 }
+    ]
+
+};
+
+//now we need to create our global context using createContext
+
+//create context 
+
+export const GlobalContext = createContext(initialState); //we are bringing GlobalContext to other files into components
+
+//in order to other components to excess to our store or global state (initial state) we need to have a provider
+
+

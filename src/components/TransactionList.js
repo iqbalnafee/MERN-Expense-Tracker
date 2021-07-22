3718b5f4385d8f32b45adf1ac2d58883a3676ec9
Nafees Iqbal <33998031+iqbalnafee@users.noreply.github.com>
2021-07-22 11:55:29 +0600

    useContext used and useContext documentation added

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,6 +1,55 @@
-import React from 'react'
+import React, { useContext } from 'react';
+
+import { GlobalContext } from '../context/GlobalState';
 
 const TransactionList = () => {
+
+    const context = useContext(GlobalContext);
+
+    //useContext documentation:
+
+    // Accepts a context object (the value returned from React.createContext) and returns the current context value for that context. The current context value is determined by the value prop of the nearest <MyContext.Provider> above the calling component in the tree.
+
+    //Below is an example of useContext
+
+    // const themes = {
+    //     light: {
+    //       foreground: "#000000",
+    //       background: "#eeeeee"
+    //     },
+    //     dark: {
+    //       foreground: "#ffffff",
+    //       background: "#222222"
+    //     }
+    //   };
+      
+    // const ThemeContext = React.createContext(themes.light);
+    
+    // function App() {
+    // return (
+    //     <ThemeContext.Provider value={themes.dark}>
+    //     <Toolbar />
+    //     </ThemeContext.Provider>
+    // );
+    // }
+    
+    // function Toolbar(props) {
+    // return (
+    //     <div>
+    //     <ThemedButton />
+    //     </div>
+    // );
+    // }
+    
+    // function ThemedButton() {
+    // const theme = useContext(ThemeContext);
+    // return (
+    //     <button style={{ background: theme.background, color: theme.foreground }}>
+    //     I am styled by theme context!
+    //     </button>
+    // );
+    // }
+
     return (
         <>
           <h3>History</h3>
